044c7dadc6be39de0625e2207daba677d264c386
Aram <aram.alaverdyan93@gmail.com>
2026-05-19 10:39:12 +0400

    Fix security issues in python code

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
+import subprocess
 
-# 1-ին ՍԽԱԼ. Գաղտնի կոդը (Token) թողել ենք հենց կոդի մեջ
-AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE"
+# 1-ԻՆ ՈՒՂՂՈՒՄ. Գաղտնի կոդը կարդում ենք Environment-ից (ոչ թե hardcode ենք անում)
+AWS_SECRET_KEY = os.environ.get("AWS_Secret_KEY")
 
 def execute_user_command(user_input):
-    # 2-րդ ՍԽԱԼ. Վտանգավոր ֆունկցիա, որը թույլ է տալիս Command Injection անել
-    os.system("echo " + user_input)
+	## 2-ՐԴ ՈՒՂՂՈՒՄ. os.system-ի փոխարեն օգտագործում ենք անվտանգ subprocess.run
+	# Սա թույլ չի տա Command Injection անել
+	subprocess.run(["echo", user_input])
 
-print("App is running successfully!")
+print("App is runnig safely and successfully!")
