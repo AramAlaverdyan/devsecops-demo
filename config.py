50e44ac8a6fc611e4a4dae15e970f6c6f322d3dc
Aram <aram.alaverdyan93@gmail.com>
2026-05-20 13:27:42 +0400

    Fix config to use env variables securely

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
-# Ֆեյք AWS բանալի՝ սկաները ստուգելու համար
-AWS_API_KEY = "AKIAIMNOBAAXYZ7EXAMPLE"
-AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+import os
+
+# Ճիշտ ու անվտանգ DevSecOps մոտեցում 2026-ում
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+>>>>>>> 5a19282 (Fix config to use env variables securely)
