4b5eca5ae600c5943c4752dbb222e5bdc926be77
Aram <aram.alaverdyan93@gmail.com>
2026-05-20 11:17:44 +0400

    Ignore low severity alerts

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ AWS_SECRET_KEY = os.environ.get("AWS_Secret_KEY")
 def execute_user_command(user_input):
 	## 2-ՐԴ ՈՒՂՂՈՒՄ. os.system-ի փոխարեն օգտագործում ենք անվտանգ subprocess.run
 	# Սա թույլ չի տա Command Injection անել
-	subprocess.run(["echo", user_input])
+	subprocess.run(["bin/echo", user_input])
 
 print("App is runnig safely and successfully!")
