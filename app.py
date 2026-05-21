4f5c4207bfa100c9eae97f53a3a7cdcb317a690d
Aram <aram.alaverdyan93@gmail.com>
2026-05-21 17:28:14 +0400

    Mask Server header and fix Clickjacking vulnerability

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
-import os
+from werkzeug.serving import WSGIRequestHandler
+
+# 🥷 ՍՈՒՏ ԻՆՖՈ. Փոխում ենք Werkzeug սերվերի պաշտոնական այցեքարտը
+WSGIRequestHandler.version_string = lambda self: "NASA-Mainframe-Secure/2026"
 
 app = Flask(__name__)
 
@@ -7,6 +10,12 @@ app = Flask(__name__)
 def home():
     return "<h1>Welcome to Secure Web App! DevSecOps 2026.</h1>"
 
+# 🛡️ ԲՈՒԺՈՒՄ ԵՆՔ CLICKJACKING-Ը. Ամեն հարցման պատասխանին կպցնում ենք անվտանգության պատերը
+@app.after_request
+def add_security_headers(response):
+    response.headers['X-Frame-Options'] = 'DENY'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
+
 if __name__ == '__main__':
-    # ԿԱՐԵՎՈՐ Է. Docker-ի ներսում պետք է լսել 0.0.0.0 պորտը, որ դրսից հասանելի լինի
-    app.run(host='0.0.0.0', port=5000) # nosec
+    app.run(host='0.0.0.0', port=5000)  # nosec
