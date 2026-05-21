1ef5748ae37d9634ccb880b0b27fa2216a6503cc
Aram <aram.alaverdyan93@gmail.com>
2026-05-21 16:49:34 +0400

    Fix Bandit medium alert with nosec and fix config syntax error

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def home():
 
 if __name__ == '__main__':
     # ԿԱՐԵՎՈՐ Է. Docker-ի ներսում պետք է լսել 0.0.0.0 պորտը, որ դրսից հասանելի լինի
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000) # nosec
--- config.py
+++ config.py
@@ -3,4 +3,3 @@ import os
 # Ճիշտ ու անվտանգ DevSecOps մոտեցում 2026-ում
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
->>>>>>> 5a19282 (Fix config to use env variables securely)
